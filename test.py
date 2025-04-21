cf374785fcb4d955bf8f16cbd8a42c21d17b44f9
UtkarshPingale <utkarshpingale11@gamil.com>
2025-04-21 22:34:53 +0530

    second

---
+++ test.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_wine
+import pandas as pd
+
+wine = load_wine()
+df = pd.DataFrame(wine.data)
+df['target'] = wine.target
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+x = df.drop('target', axis=1)
+y = df['target']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.2)
+
+model = LogisticRegression(max_iter=200)
+model.fit(x_train, y_train)
+pred = model.predict(x_test)
+print(f"{accuracy_score(pred, y_test)}")
